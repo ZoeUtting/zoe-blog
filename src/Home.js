0d859bec89af39509dce3eeb74ea1f0975c28b29
ZoeUttingTecharge <zoe.utting@techarge.co.uk>
2023-11-25 18:11:45 +0000

    lesson 7 click handling

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,17 @@
 const Home = () => {
+
+const handleClick = (e) => {
+    console.log('hello beech', e);
+}
+const handleClickAgain =(name, e) => {
+    console.log('hello' + name, e.target)
+}
+
     return ( 
         <div className="homepage">
             <h2>homepage</h2>
+            <button onClick={handleClick}>Click me</button>
+                <button onClick={(e) => handleClickAgain('zoe', e)}>Click me 2</button>
         </div>
      );
 }

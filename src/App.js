26e0cb4104d558517672f67dcafdfd6a8c3aab42
ZoeUttingTecharge <zoe.utting@techarge.co.uk>
2023-11-25 17:21:32 +0000

    End of tutorial 4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 
 function App() {
+  const title ='Welcome to my blog'  
+  const likes = 50
+  const link ="https://www.zoeutting.com"
+  // const person = {name: 'zoe', age: 26}
+  
   return (
     <div className="App">
     <div className="content">
-      <h1>App component</h1>
+      <h1>{title}</h1>
+      {/* {person.name} */}
+      {likes}
+      <p>{10 * 2}</p>
+      <a href={link}>zoe website</a>
     </div>
     </div>
   );

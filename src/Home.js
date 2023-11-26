556ec233bc831e178f2174cf61b0698d047aff69
ZoeUttingTecharge <zoe.utting@techarge.co.uk>
2023-11-26 17:00:13 +0000

    Mapping Blog array with some basic css

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,18 @@
 
 import { useState } from "react"
 const Home = () => {
-const[name, setName] = useState('MARIO');
-const handleClick = () => {
-    setName('zoe');
-}
-const handleClickAgain =(name, e) => {
-    console.log('hello' + name, e.target)
-}
-
+ const [blogs, setBlogs]
+ = useState([ { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+ { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+ { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }]);
     return ( 
         <div className="homepage">
-            <h2>homepage</h2>
-            { name }
-            <button onClick={handleClick}>Click me</button>
-                <button onClick={(e) => handleClickAgain('zoe', e)}>Click me 2</button>
+        {blogs.map((blog) => (
+            <div className="blog-preview" key={blog.id}>
+                <h2>{blog.title}</h2>
+                <p>Written by {blog.author}</p>
+            </div>
+        ))}
         </div>
      );
 }

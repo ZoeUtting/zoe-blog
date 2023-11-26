762c42f9bfadeceb513e0138152ad281a499c238
ZoeUttingTecharge <zoe.utting@techarge.co.uk>
2023-11-26 16:43:02 +0000

    Stuff from lesson 8 about states

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,9 @@
-const Home = () => {
 
-const handleClick = (e) => {
-    console.log('hello beech', e);
+import { useState } from "react"
+const Home = () => {
+const[name, setName] = useState('MARIO');
+const handleClick = () => {
+    setName('zoe');
 }
 const handleClickAgain =(name, e) => {
     console.log('hello' + name, e.target)
@@ -10,6 +12,7 @@ const handleClickAgain =(name, e) => {
     return ( 
         <div className="homepage">
             <h2>homepage</h2>
+            { name }
             <button onClick={handleClick}>Click me</button>
                 <button onClick={(e) => handleClickAgain('zoe', e)}>Click me 2</button>
         </div>

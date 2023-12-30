40e2470298be49d042233353abf90adab5a962ac
ZoeUtting <z.utting@ntlworld.com>
2023-12-30 11:46:53 +0000

    Handling fetch errors

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,7 @@ import BlogList from "./BlogList";
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   //  const handleDelete = (id) =>{
   //     const newBlogs = blogs.filter((blog) => blog.id !== id);
@@ -13,18 +14,28 @@ const Home = () => {
     setTimeout(() => {
       fetch("http://localhost:8000/blogs")
         .then((res) => {
+          console.log(res);
+          if (!res.ok) {
+            throw Error('Could not fetch data');
+          }
           return res.json();
         })
         .then((data) => {
           console.log(data);
           setBlogs(data);
           setIsLoading(false);
+          setError(null);
+        })
+        .catch(err => {
+          setIsLoading(false);
+          setError(err.message);
         });
     }, 1000);
   }, []);
 
   return (
     <div className="homepage">
+      {error && <div>{error}</div>}
       {isLoading && <div>LOADING</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs" />}
     </div>

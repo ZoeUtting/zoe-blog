99421b37c26c46f17b074777e1326acc91360023
ZoeUtting <z.utting@ntlworld.com>
2024-01-07 15:21:33 +0000

    updates

--- src/Home.js
+++ src/Home.js
@@ -1,38 +1,11 @@
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const {data: blogs, isLoading, error} = useFetch("http://localhost:8000/blogs")
 
-  //  const handleDelete = (id) =>{
-  //     const newBlogs = blogs.filter((blog) => blog.id !== id);
-  //     setBlogs(newBlogs);
-  //  }
 
-  useEffect(() => {
-    setTimeout(() => {
-      fetch("http://localhost:8000/blogs")
-        .then((res) => {
-          console.log(res);
-          if (!res.ok) {
-            throw Error('Could not fetch data');
-          }
-          return res.json();
-        })
-        .then((data) => {
-          console.log(data);
-          setBlogs(data);
-          setIsLoading(false);
-          setError(null);
-        })
-        .catch(err => {
-          setIsLoading(false);
-          setError(err.message);
-        });
-    }, 1000);
-  }, []);
-
+  
   return (
     <div className="homepage">
       {error && <div>{error}</div>}
